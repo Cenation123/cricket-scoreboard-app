23bf8684b5aaacbfbcba23da2f0edaeaa60bbc62
Cenation123 <114369054+Cenation123@users.noreply.github.com>
2023-07-07 16:29:56 +0530

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,14 +1,14 @@
 let overs_btn = document.getElementById("overs_btn");
 let bolwed_btn = document.getElementById("bolwed_btn");
 let ball_btn = document.getElementById("ball_btn");
-
+let reset_btn = document.getElementById("retset_btn");
 let over = document.getElementById("overs");
 let Bolwed = document.getElementById("bolwed");
 let ball = document.getElementById("ball_bolwed");
 let run = document.getElementById("run");
 let delivery = document.getElementById("delivery");
 let total = document.getElementById("total");
-
+let resett = document.getElementById("reset");
 let overss = 0;
 let bolweds = 0;
 let Ball = 0;
@@ -31,6 +31,9 @@ function bolwed() {
     bolweds = 0;
     Bolwed.innerText = "all out";
   }
+  if (Del == 6) {
+    delivery.innerText = 0;
+  }
 }
 
 function balls() {
@@ -39,16 +42,24 @@ function balls() {
   Run = Math.floor(Math.random() * 7) + 0;
   run.innerText = Run;
   Del = run.innerText;
-  Total +=Run ;
+  Total += Run;
   total.innerText = Total;
-  delivery.innerText += Del;
-  if (Del == 6) {
-    Del = run.innerText = 0;
-  }
+  delivery.innerText += Run;
+
   if (Ball == 6) {
     overss += 1;
     over.innerText = overss;
     Ball = 0;
+    // Del = run.innerText = 0;
     ball.innerText = "over";
   }
+}
+
+function reset() {
+  over.innerText = 0;
+  Bolwed.innerText = 0;
+  ball.innerText = 0;
+  run.innerText = 0;
+  delivery.innerText = 0;
+  total.innerText = 0;
 }
